5770b3e55181a097dca54a935f3fa98cecd27179
Ishu Tripathi <tripathiishu16@gmail.com>
2017-10-30 19:50:12 +0530

    commentings in login and sign up

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,3 +1,5 @@
+//import all the dependencies
+
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from 'angular4-social-login';
@@ -15,11 +17,11 @@ import { ProjectMember } from "../shared/model/projectMember";
 })
 export class RegisterComponent implements OnInit {
 
-  user: SocialUser;
+  user: SocialUser;    
   details:any[]=[];
   userdetails:any;
   index:Login;
-  private model = {
+  private model = {             //Binding with html 
     FirstName:'',
     LastName:'',  
     Organization:'',  
@@ -28,30 +30,30 @@ export class RegisterComponent implements OnInit {
    Password:''
     };
     ConfirmPassword:string;
-    projectmember:ProjectMember={
+    projectmember:ProjectMember={               //object type of Projectmember
       ProjectId:null,
     MemberId:null,
     ActAs:null
     }
   
-  
+  //inject sevices on which this component depends
     constructor(private authService: AuthService,private router:Router,private loginservice:LoginService,private route:ActivatedRoute) { }
   
     ngOnInit() {
-      this.authService.authState.subscribe((user) => {
+      this.authService.authState.subscribe((user) => {                
         this.user = user;
        if(this.user != null){ this.router.navigate(["app-dashboard"])}
       });
-      this.loginservice.getAll().subscribe(details=>{
-        this.details=details;
+      this.loginservice.getAll().subscribe(details=>{         //calling getall function of loginservice 
+        this.details=details;                                 //and return details of all users
       })
       this.route.params.subscribe((param) => 
-      this.projectmember.ProjectId = +param['id']);
+      this.projectmember.ProjectId = +param['id']);               //getting project id from route
       console.log("helllooo",this.projectmember.ProjectId);
     }
   
     signInWithGoogle(): void {
-      this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
+      this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)   //calling signIn function of authoservice
                       .then(data =>{ console.log(data)
                         this.authService.authState.subscribe((user) => {
                           this.user = user;
@@ -60,7 +62,7 @@ export class RegisterComponent implements OnInit {
     }
   
     signInWithFB(): void {
-      this.authService.signIn(FacebookLoginProvider.PROVIDER_ID)
+      this.authService.signIn(FacebookLoginProvider.PROVIDER_ID)    //calling signIn function from authservice
       .then(data =>{ console.log(data)
         this.authService.authState.subscribe((user) => {
           this.user = user;
@@ -72,20 +74,20 @@ export class RegisterComponent implements OnInit {
       this.authService.signOut();
     }
     
-    CreateAccount(){
+    CreateAccount(){                        //registering the user
 
       
-      this.index=this.details.find((m)=>m.email==this.model.Email);
+      this.index=this.details.find((m)=>m.email==this.model.Email);  //find the details of a particular user 
      
       
       if(this.model.Department==''||this.model.Email==''||this.model.FirstName==''||this.model.LastName==''||this.model.Organization==''||this.model.Password==''){
-        swal('','Enter the Required fields','error');
+        swal('','Enter the Required fields','error');             //if any entry is empty then show the alert 
       }
      else if (!this.model.Email.includes("@"&&".")){
-      swal('','Enter valid email-address','error');  
+      swal('','Enter valid email-address','error');               //if emailid does not contains @ and . then show the alert
     }
     else if(this.model.Password != this.ConfirmPassword){
-      swal('','confirm password does not match the password','error')
+      swal('','confirm password does not match the password','error')    //if confirm and password is not equal then show password does not match
     }
    
      
@@ -94,23 +96,23 @@ export class RegisterComponent implements OnInit {
          if((this.projectmember.ProjectId != null))
           {
             this.projectmember.ActAs=1;
-      this.loginservice.postLoginDetails(this.model).then(data=>this.loginservice.get(this.model.Email).then(detail=>{
-        this.userdetails=detail;
-        this.projectmember.MemberId=this.userdetails.id;
+      this.loginservice.postLoginDetails(this.model).then(data=>this.loginservice.get(this.model.Email).then(detail=>{  //posting the details of user 
+        this.userdetails=detail;                                                                                        //then calling the get method to find the user unique id  
+        this.projectmember.MemberId=this.userdetails.id;                                                                //then posting the user id and team id
         console.log('gsgsgsgs',this.userdetails);
       })).then(project=>this.loginservice.postMemberDetails(this.projectmember)).then(
       msg=> swal('','Your account has been created','success'));
 
-      this.router.navigateByUrl('/app-signup');
+      this.router.navigateByUrl('/app-signup');     //navigate to the signup page
           }
           else{
-            this.loginservice.postLoginDetails(this.model);
+            this.loginservice.postLoginDetails(this.model);       //calling post method to register the details       
             swal('','Your account has been created','success');
-            this.router.navigateByUrl('/app-signup');
+             this.router.navigateByUrl('/app-signup');      //navigate to the signup page
           }
        }
        else{
-         swal('','Email Already Exists','error')
+         swal('','Email Already Exists','error')      //if enter id matches with the existing id in database 
        } 
     }
      
--- server/MyNeo4j/Repository/ProjectMemberRepository.cs
+++ server/MyNeo4j/Repository/ProjectMemberRepository.cs
@@ -6,30 +6,31 @@ using System.Threading.Tasks;
 
 namespace MyNeo4j.Service
 {
+    //interface of project member class
     public interface IProjectMemberRepository
     {
         ProjectMember getMemberDetails(int id);
         void Add_MemberDetails(ProjectMember member);
-
-
     }
     public class ProjectMemberRepository : IProjectMemberRepository
     {
+        //object of dbcontext class
         public Neo4jDbContext _context;
+        //constructor
         public ProjectMemberRepository(Neo4jDbContext context)
         {
             _context = context;
         }
-
+        //this method gets the details of particular project member
         public ProjectMember getMemberDetails(int id)
         {
             return _context.Projectmember.FirstOrDefault(p => p.id == id);
         }
+        //this method adds the details of particular member corresponding to projectid
         public void Add_MemberDetails(ProjectMember member)
         {
             _context.Projectmember.Add(member);
             _context.SaveChanges();
         }
-
     }
 }
