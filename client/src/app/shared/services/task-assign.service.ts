f4281817018609ae05ac1fcaf515a167f30fd1e8
aabhaas <aabhaas.5.malhotra@niit-tech.com>
2017-10-31 23:02:43 -0700

    task-assign update

--- client/src/app/shared/services/task-assign.service.ts
+++ client/src/app/shared/services/task-assign.service.ts
@@ -10,33 +10,33 @@ export class TaskAssignService {
   constructor(private http : Http) { } //inject http 
  //get the list of tasks accordingly their particular sprint
   getTaskList(id:number):Promise<any>{
-    return this.http.get("http://localhost:52258/api/TaskBacklog/GetTask/"+id)
+    return this.http.get("http://localhost:52257/api/TaskBacklog/GetTask/"+id)
                     .toPromise()
                     .then(Response => Response);//return the response
 
   }
 //get the list of teams in the particular project by its projectId
   getTeamList(id:number):Promise<any>{
-    return this.http.get("http://localhost:52258/api/TaskBacklog/GetByTeamId/"+id)
+    return this.http.get("http://localhost:52257/api/TaskBacklog/GetByTeamId/"+id)
                     .toPromise()
                     .then(Response => Response);
 
   }
 //get the list of members in a particular team for assigning task
   getTeamMemberList(id:number):Promise<any>{
-    return this.http.get("http://localhost:52258/api/TaskBacklog/GetTeamMember/"+id)
+    return this.http.get("http://localhost:52257/api/TaskBacklog/GetTeamMember/"+id)
                     .toPromise()
                     .then(Response => Response);
 
   }
 //update methos that updates with the values of the members assigned to a particular task
   assignTask(object:Members,id:number):Promise<any>{
-    return this.http.put("http://localhost:52258/api/TaskBacklog/"+id,object,{headers: new Headers({ 'Content-Type': 'application/json'})})
+    return this.http.put("http://localhost:52257/api/TaskBacklog/"+id,object,{headers: new Headers({ 'Content-Type': 'application/json'})})
                     .toPromise();
   }
 //get the name of the members that are assigned to a particular task
   getName(id:number){
-       return this.http.get("http://localhost:52258/api/TaskBacklog/GetMemberName/"+id)
+       return this.http.get("http://localhost:52257/api/TaskBacklog/GetMemberName/"+id)
                    .map(Response => Response);
   }
 }
